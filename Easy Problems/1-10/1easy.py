b70ff2c8108682fa1d57d3f0831e832623bce638
Lucas-Guimaraes <lucasthirdg@gmail.com>
2020-10-31 18:28:26 -0700

    Create 1easy.py

---
+++ Easy Problems/1-10/1easy.py
@@ -0,0 +1,8 @@
+#https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+
+user_name = raw_input("Put in your name: ")
+user_age = raw_input("Whhat's your age?: ")
+user_screenname = raw_input("How about a username?: ")
+
+print("Your name is " + user_name + " your are " + user_age + " years old, and your username is " + user_screenname)
+raw_input()
